86470c1a0f5112cf8b0f0218a6aeb06ede343623
BRUWOLF <am.bruneza@gmail.com>
2022-07-27 16:17:55 +0200

    finished popup for Link

--- JS/PopUp/pop-iframe.js
+++ JS/PopUp/pop-iframe.js
@@ -1,29 +1,84 @@
+function validURL(str) {
+  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return !!pattern.test(str);
+}
 
-let clickedlink = document.querySelector(".bru-poplink");
+function popStyle(){
+	var head = document.getElementsByTagName('HEAD')[0];
+        // Create new link Element
+        var link = document.createElement('link');
+        // set the attributes for link element
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = 'style.css';
+        head.appendChild(link);
+}
+function bruPop (targetIdentifier, action){
+	// targetElement.preventDefault();
 
-window.console.log(clickedlink);
+	let targetElement = document.querySelector(targetIdentifier);
+	let target = targetElement.href;
+	let targetContent;
+	let targettype;
 
-clickedlink.addEventListener("click", bruPop);
-
-function bruPop (event){
-
-		event.preventDefault();
-		let targetLink = event.target.href;
-    let pframe= document.createElement('iframe');
-
-    pframe.style.cssText = 'width: 70%;	height: 70%; border:0;';
-	pframe.setAttribute('src', `${targetLink}`)
-	pframe.innerHTML = 'allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"';
+	let popContainer = document.createElement('div');
+	popContainer.classList.add("popup-container");
 
 	let popBox = document.createElement('div');
-	popBox.classList.add("pop-link-box");
-	popBox.appendChild(pframe);
+	popBox.classList.add("popup-box");
+
+	let popFrame= document.createElement('iframe');
+	popFrame.style.cssText = 'width: 100%; height: 100%; border:0;';
+	popFrame.innerHTML = 'allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"';
 
 
-	event.target.parentNode.appendChild(popBox);
+	let types = ['id:','class:',,'map-location:','link:'];
 
+	types.forEach( function(t) {
+		if(target.includes(t)){
+			targettype = t;
+			targetContent = target.replace(t,'');
+		}
+	});
+
+	window.console.log("Content: "+ targetContent + " Type: " + targettype);
+
+	if(!targettype){
+		window.console.log('Invalid Option ');
+		return false;
+	}
+
+	switch (targettype) {
+		case "link:":
+			if(!validURL(targetContent)){
+				window.console.log('Invalid Link.');
+				return false;
+			}
+
+			popFrame.setAttribute('src', `${targetContent}`);
+			popBox.appendChild(popFrame);
+
+			break;
+		default:
+			window.console.log('type of content not found.');
+			break;
+	}
+
+	popContainer.appendChild(popBox);
+
+	targetElement.parentNode.appendChild(popContainer);
+	window.console.log(popContainer);
+
+	/*
 	popBox.addEventListener("click", function() {
-			event.parentNode.removeChild(popBox);
+		event.parentNode.removeChild(popBox);
 	}
 	);
-	}
+	*/
+
+}
